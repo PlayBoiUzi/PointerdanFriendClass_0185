fd74823d1c70bcec064c67aa64f7833f81208642
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-05-05 16:17:46 +0700

    Defenisi class mahasiswa

--- friendFuntionsatu.cpp
+++ friendFuntionsatu.cpp
@@ -11,15 +11,3 @@ class mahasiswa
        friend void setNama(mahasiswa &a, string);
 };
 
-void setNama(mahasiswa &a, string b)
-{
-    a.nama = b;
-    cout << a.nama;
-}
-
-int main()
-{
-    mahasiswa joko;
-    setNama(joko, "Kairi Kumar");
-    return 0;
-}
