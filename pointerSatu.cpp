5984bcca7cb656228b24c33ad4e0e92bac0280d8
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-05-05 15:33:48 +0700

    membuat objek mahasiswa dan akses member

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -10,3 +10,18 @@ class mahasiswa {
 
 };
 
+int main (){
+
+    mahasiswa mhs; //object mhs
+    mhs.nim = 5;
+    mhs.shownim(); //member acces operator
+
+    mahasiswa &refMhs = mhs; //pointer reference refMhs
+    refMhs.nim = 2; //member acces operator
+    whs.shownim();
+    
+    mahasiswa *pMhs = &mhs; //pointer reference pMhs
+    pMhs->nim = 3; //arrow operator
+    pMhs ->shownim();
+    return 0;
+}
